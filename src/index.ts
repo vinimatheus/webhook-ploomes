b0c861f8614918ec093454a8d05948ebde740c04
vinimatheus <vinimatheus999@gmail.com>
2024-10-24 17:48:03 -0300

    update vercel

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import fastify from 'fastify'
+import fastify from "fastify";
 
-const app = fastify()
+const app = fastify();
 
-app.get('/', async () => {
-  return { hello: 'world' }
-})
+
+app.get("/", async () => {
+  return { hello: "world" };
+});
 
 app.listen({ port: 3333 }).then(() => {
-  console.log('🔥 Server running on port 3333')
-})
+  console.log("🔥 Server running on port 3333");
+});
