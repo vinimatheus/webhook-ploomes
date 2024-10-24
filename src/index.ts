c572b92141c3d396711bf0a7f37884cf7716291d
vinimatheus <sistemas@profood.com.br>
2024-10-24 14:55:07 -0300

    init project 🔥

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/', async () => {
+  return { hello: 'world' }
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('🔥 Server running on port 3333')
+})
